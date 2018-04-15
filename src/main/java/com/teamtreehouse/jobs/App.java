a3134a727dac13349e97af89906a2ca759be3a34
Trevor Davis <tzendou@gmail.com>
2018-04-15 15:00:26 -0700

    Add filter, limit, collect examples

--- src/main/java/com/teamtreehouse/jobs/App.java
+++ src/main/java/com/teamtreehouse/jobs/App.java
@@ -4,27 +4,69 @@ import com.teamtreehouse.jobs.model.Job;
 import com.teamtreehouse.jobs.service.JobService;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
 
-  public static void main(String[] args) {
-    JobService service = new JobService();
-    boolean shouldRefresh = false;
-    try {
-      if (shouldRefresh) {
-        service.refresh();
-      }
-      List<Job> jobs = service.loadJobs();
-      System.out.printf("Total jobs:  %d %n %n", jobs.size());
-      explore(jobs);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public static void main(String[] args) {
+        JobService service = new JobService();
+        boolean shouldRefresh = false;
+        try {
+            if (shouldRefresh) {
+                service.refresh();
+            }
+            List<Job> jobs = service.loadJobs();
+            System.out.printf("Total jobs:  %d %n %n", jobs.size());
+            explore(jobs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private static void explore(List<Job> jobs) {
-    // Your amazing code below...
+    private static void explore(List<Job> jobs) {
+        // Your amazing code below...
+        getThreeJuniorJobsStream(jobs).forEach(System.out::println);
+    }
 
-  }
+    private static boolean isJuniorJob(Job job) {
+        String title = job.getTitle().toLowerCase();
+        return (title.contains("junior") || title.contains("jr"));
+    }
+
+    private static List<Job> getThreeJuniorJobsStream(List<Job> jobs) {
+        return jobs.stream()
+                .filter(App::isJuniorJob)
+                .limit(3)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Job> getThreeJuniorJobsImperatively(List<Job> jobs) {
+        List<Job> juniorJobs = new ArrayList<>();
+        for (Job job : jobs) {
+            if (isJuniorJob(job)) {
+                juniorJobs.add(job);
+                if (juniorJobs.size() >= 3) {
+                    break;
+                }
+            }
+        }
+        return juniorJobs;
+    }
+
+    private static void printPortlandJobsStream(List<Job> jobs) {
+        jobs.stream()
+                .filter(job -> job.getState().equals("OR"))
+                .filter(job -> job.getCity().equals("Portland"))
+                .forEach(System.out::println);
+    }
+
+    private static void printPortlandJobsImperatively(List<Job> jobs) {
+        for (Job job : jobs) {
+            if (job.getState().equals("OR") && job.getCity().equals("Portland")) {
+                System.out.println(job);
+            }
+        }
+    }
 }
--- src/main/java/com/teamtreehouse/jobs/service/JobService.java
+++ src/main/java/com/teamtreehouse/jobs/service/JobService.java
@@ -9,7 +9,6 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Key;
-
 import com.teamtreehouse.jobs.model.Job;
 import com.teamtreehouse.jobs.model.ResultsPage;
 
@@ -26,120 +25,119 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toList;
 
 public class JobService {
-  private static final String CACHE_PATH = "./src/main/resources/cached";
-  static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-  static final JsonFactory JSON_FACTORY = new JacksonFactory();
+    static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    static final JsonFactory JSON_FACTORY = new JacksonFactory();
+    private static final String CACHE_PATH = "./src/main/resources/cached";
 
-  public static class IndeedUrl extends GenericUrl {
-    @Key
-    public String publisher;
-
-    @Key
-    public String format = "json";
-
-    @Key
-    public String v = "2";
-
-    @Key
-    public String fromage = "1";  // 1 day old, not French for cheese
-
-    @Key
-    public String q;
-
-    @Key
-    public int start;
-
-    @Key
-    public int limit = 25;
-
-    public IndeedUrl() {
-      super("http://api.indeed.com/ads/apisearch");
+    public void refresh() throws IOException {
+        cache(search());
     }
 
-  }
+    public List<HttpResponse> search() throws IOException {
+        JsonObjectParser parser = new JsonObjectParser.Builder(JSON_FACTORY).build();
+        HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(req -> req.setParser(parser));
+        Properties config = new Properties();
+        config.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+        String publisher = config.getProperty("api.indeed.publisher");
+        // Execute a quick search just to get the total
+        IndeedUrl estimateUrl = new IndeedUrl();
+        estimateUrl.publisher = publisher;
+        estimateUrl.q = "java";
+        estimateUrl.limit = 1;
+        ResultsPage estimate = requestFactory
+                .buildGetRequest(estimateUrl)
+                .execute()
+                .parseAs(ResultsPage.class);
 
-  public void refresh() throws IOException {
-    cache(search());
-  }
+        System.out.println("Estimated results: " + estimate.totalResults);
+        return IntStream.range(0, Math.min(estimate.totalResults, 1000)).parallel()
+                .filter(start -> start % ResultsPage.MAX_PER_PAGE == 0)
+                .mapToObj(start -> {
+                    IndeedUrl url = new IndeedUrl();
+                    url.publisher = publisher;
+                    url.q = "java";
+                    url.start = start;
+                    url.limit = ResultsPage.MAX_PER_PAGE;
+                    try {
+                        return requestFactory.buildGetRequest(url);
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
+                .map(request -> {
+                    try {
+                        return request.execute();
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
+                .collect(toList());
+    }
 
-  public List<HttpResponse> search() throws IOException {
-    JsonObjectParser parser = new JsonObjectParser.Builder(JSON_FACTORY).build();
-    HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(req -> req.setParser(parser));
-    Properties config = new Properties();
-    config.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
-    String publisher = config.getProperty("api.indeed.publisher");
-    // Execute a quick search just to get the total
-    IndeedUrl estimateUrl = new IndeedUrl();
-    estimateUrl.publisher = publisher;
-    estimateUrl.q = "java";
-    estimateUrl.limit = 1;
-    ResultsPage estimate = requestFactory
-            .buildGetRequest(estimateUrl)
-            .execute()
-            .parseAs(ResultsPage.class);
+    public void cache(List<HttpResponse> responses) throws IOException {
+        // Remove all files in the cache
+        Files.walk(Paths.get(CACHE_PATH))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .forEach(File::delete);
 
-    System.out.println("Estimated results: " + estimate.totalResults);
-    return IntStream.range(0, Math.min(estimate.totalResults, 1000)).parallel()
-            .filter(start -> start % ResultsPage.MAX_PER_PAGE == 0)
-            .mapToObj(start -> {
-              IndeedUrl url = new IndeedUrl();
-              url.publisher = publisher;
-              url.q = "java";
-              url.start = start;
-              url.limit = ResultsPage.MAX_PER_PAGE;
-              try {
-                return requestFactory.buildGetRequest(url);
-              } catch (IOException e) {
-                throw new UncheckedIOException(e);
-              }
-            })
-            .map(request -> {
-              try {
-                return request.execute();
-              } catch (IOException e) {
-                throw new UncheckedIOException(e);
-              }
-            })
-            .collect(toList());
-  }
+        // Loop through responses and drop them out here
+        IntStream.range(0, responses.size()).parallel()
+                .forEach(i -> {
+                    HttpResponse response = responses.get(i);
+                    try {
+                        response.download(
+                                Files.newOutputStream(Paths.get(CACHE_PATH, "jobs-" + (i + 1) + ".json")));
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                });
+    }
 
-  public void cache(List<HttpResponse> responses) throws IOException {
-    // Remove all files in the cache
-    Files.walk(Paths.get(CACHE_PATH))
-            .filter(Files::isRegularFile)
-            .map(Path::toFile)
-            .forEach(File::delete);
+    public List<Job> loadJobs() throws IOException {
+        JsonObjectParser parser = new JsonObjectParser.Builder(JSON_FACTORY).build();
+        List<ResultsPage> pages = Files.walk(Paths.get(CACHE_PATH))
+                .filter(Files::isRegularFile)
+                .map(path -> {
+                    try {
+                        return parser.parseAndClose(Files.newBufferedReader(path), ResultsPage.class);
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
+                .collect(toList());
+        return pages.stream()
+                .flatMap(page -> page.jobs.stream())
+                .collect(toList());
+    }
 
-    // Loop through responses and drop them out here
-    IntStream.range(0, responses.size()).parallel()
-            .forEach(i -> {
-              HttpResponse response = responses.get(i);
-              try {
-                response.download(
-                        Files.newOutputStream(Paths.get(CACHE_PATH, "jobs-" + (i + 1) + ".json")));
-              } catch (IOException e) {
-                throw new UncheckedIOException(e);
-              }
-            });
-  }
+    public static class IndeedUrl extends GenericUrl {
+        @Key
+        public String publisher;
 
-  public List<Job> loadJobs() throws IOException {
-    JsonObjectParser parser = new JsonObjectParser.Builder(JSON_FACTORY).build();
-    List<ResultsPage> pages = Files.walk(Paths.get(CACHE_PATH))
-            .filter(Files::isRegularFile)
-            .map(path -> {
-              try {
-                return parser.parseAndClose(Files.newBufferedReader(path), ResultsPage.class);
-              } catch (IOException e) {
-                throw new UncheckedIOException(e);
-              }
-            })
-            .collect(toList());
-    return pages.stream()
-            .flatMap(page -> page.jobs.stream())
-            .collect(toList());
-  }
+        @Key
+        public String format = "json";
 
+        @Key
+        public String v = "2";
+
+        @Key
+        public String fromage = "1";  // 1 day old, not French for cheese
+
+        @Key
+        public String q;
+
+        @Key
+        public int start;
+
+        @Key
+        public int limit = 25;
+
+        public IndeedUrl() {
+            super("http://api.indeed.com/ads/apisearch");
+        }
+
+    }
 
 
 }
